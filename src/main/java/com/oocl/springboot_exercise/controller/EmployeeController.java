b9f7e5ee4eb229975803804559092b0f1768af0f
HEHE7 <hehe7@oocl.com>
2025-08-26 18:36:06 +0800

    feat: 增加根据gender查询Employee和根据id更新Employee的接口

--- src/main/java/com/oocl/springboot_exercise/controller/EmployeeController.java
+++ src/main/java/com/oocl/springboot_exercise/controller/EmployeeController.java
@@ -2,8 +2,11 @@ package com.oocl.springboot_exercise.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/employees")
@@ -12,17 +15,38 @@ public class EmployeeController {
     private final static Map<Integer, Employee> db = new HashMap<>();
 
     @PostMapping
-    public void add(@RequestBody Employee employee){
+    public void add(@RequestBody Employee employee) {
         employee.setId(db.size() + 1);
-        db.put(db.size() +1, employee);
+        db.put(db.size() + 1, employee);
     }
 
     @GetMapping("/{id}")
-    public Employee getById(@PathVariable("id") int id){
+    public Employee getById(@PathVariable("id") int id) {
         return db.get(id);
     }
 
+    @GetMapping
+    public List<Employee> getEmployeeByGender(@RequestParam(required = false) String gender) {
+        List<Employee> employees = new ArrayList<>(db.values());
+        if (gender != null) {
+            return employees.stream()
+                    .filter(employee -> gender.equals(employee.getGender()))
+                    .collect(Collectors.toList());
+        }
+        return employees;
+    }
 
-
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Integer id, @RequestBody Employee employee){
+        Employee oldEmployee = db.get(id);
+        if(oldEmployee != null){
+            oldEmployee.setGender(employee.getGender());
+            oldEmployee.setAge(employee.getAge());
+            oldEmployee.setName(employee.getName());
+            oldEmployee.setSalary(employee.getSalary());
+            db.put(id, oldEmployee);
+        }
+        return oldEmployee;
+    }
 
 }
